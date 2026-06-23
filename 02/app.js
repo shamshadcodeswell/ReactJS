d4da0c33001c355eaa39260d565fed4e64b9e1a0
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-06-23 21:59:44 +0530

    setting up a react app

---
+++ 02/app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+/*
+<div id = "parent">
+<div id = "child">
+<h1 class = "heading1">
+<h2 class = "heading2">
+</div>
+</div>
+*/
+
+const element = React.createElement(
+  "div",
+  { id: "parent" },
+  //   React.createElement("div", { id: "child" }, [
+  //     React.createElement("h1", { id: "heading1" }, "heading1"),
+  //     React.createElement("h2", { id: "heading2" }, "heading2"),
+  //   ]),
+  "helloWorld",
+);
+console.log(element);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
