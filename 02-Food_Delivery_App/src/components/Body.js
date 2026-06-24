81fd3fdcc76d70df32a32e14e5a8a019e35a10f0
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-06-24 20:58:17 +0530

    added a button filter to filter out restaurant with >4 rating

--- 02-Food_Delivery_App/src/components/Body.js
+++ 02-Food_Delivery_App/src/components/Body.js
@@ -1,6 +1,7 @@
 import dataList from "../../utils/mock-data";
+import { useState } from "react";
 const Card = (props) => {
-  const { image, name } = props.dataList;
+  const { image, name, rating } = props.dataList;
   return (
     <div className="card">
       <img
@@ -9,17 +10,29 @@ const Card = (props) => {
         src={image}
       ></img>
       <h3>{name}</h3>
+      <h3>{rating}</h3>
     </div>
   );
 };
 
 const Body = () => {
+  const [resList, setResList] = useState(dataList);
   return (
     <div className="app-body">
       <h2>search</h2>
       <h2>Restaurants Near You</h2>
+      <button
+        className="button"
+        onClick={() => {
+          const filteredResList = resList.filter((res) => res.rating > 4);
+          setResList(filteredResList);
+        }}
+      >
+        {" "}
+        Filter Top Restaurants{" "}
+      </button>
       <div className="card-container">
-        {dataList.map((restaurant) => (
+        {resList.map((restaurant) => (
           <Card key={restaurant.position} dataList={restaurant} />
         ))}
       </div>
